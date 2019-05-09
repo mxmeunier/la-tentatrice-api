fc58b78945aa021a919d9b1015f2140ed0e937d0
maxime-meunier <maxime.meunier@forcity.com>
2019-05-09 16:00:14 +0200

    core: add post method for ingredients

--- app.js
+++ app.js
@@ -4,12 +4,34 @@ const db = require('./db/db');
 // Set up the express app
 const app = express();
 
+app.use(express.json());
+
 // Get all ingredients
 app.get('/api/ingredients', (req, res) => {
 	res.status(200).send({
 		success: 'true',
 		message: 'ingredients retrived successfully',
 		ingredients: db
+	});
+});
+
+app.post('/api/ingredients', (req, res) => {
+	console.log(req.body);
+	if(!req.body.name) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'name is required'
+    });
+  };
+	const ingredient = {
+		id: db.ingredients.length + 1,
+		name: req.body.name
+	};
+	db.ingredients.push(ingredient);
+	return res.status(201).send({
+		success: 'true',
+		message: 'ingredient added successfully',
+		ingredient
 	});
 });
 
